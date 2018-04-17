70d4aee64f768b9c239f75395cc15b6f473024ad
ben@gmail.com <bdu38eicm>
2018-04-17 23:58:06 +0200

    updated dependencies

---
+++ rest/src/main/kotlin/com/bendsoft/multitrackrecorder/MultitrackrecorderApplication.kt
@@ -0,0 +1,11 @@
+package com.bendsoft.multitrackrecorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultitrackrecorderApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultitrackrecorderApplication>(*args)
+}
