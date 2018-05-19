d3fbab921757dba623aa0bc2f5c46b253f328ec0
bendudler <ben.dudler@gmail.com>
2018-05-19 12:30:56 +0200

    wip - add timer to recorder

--- src/app/recorder/recorder.component.ts
+++ src/app/recorder/recorder.component.ts
@@ -13,11 +13,46 @@ export class RecorderComponent implements OnInit {
   recordingDate = new FormControl(moment(), Validators.required);
   isRecording = false;
 
+  timer = new RecordingTimer();
+
   constructor() {}
 
   ngOnInit() {}
 
   toggleRecording() {
     this.isRecording = !this.isRecording;
+
+    if (this.isRecording) {
+      this.timer.startWatch();
+    } else {
+      this.timer.stopWatch();
+    }
+  }
+
+}
+
+// TODO: Transform to own component
+class RecordingTimer {
+  public static readonly timeFormat = 'HH : mm : ss';
+  public static readonly intervalPrecision = 1000;
+
+  private _currentValue = moment().hour(0).minute(0).second(0);
+  public formattedValue = this._currentValue.format(RecordingTimer.timeFormat);
+
+  private runningClock;
+
+  constructor() {}
+
+  private displayTime() {
+    this._currentValue.add(RecordingTimer.intervalPrecision, 'ms');
+    this.formattedValue = this._currentValue.format(RecordingTimer.timeFormat);
+  }
+
+  startWatch() {
+    this.runningClock = setInterval(this.displayTime.bind(this), RecordingTimer.intervalPrecision);
+  }
+
+  stopWatch() {
+    clearInterval(this.runningClock);
   }
 }
