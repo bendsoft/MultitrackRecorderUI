ee2fa6c54789252e4bac0aaff92b4c73f7c8a754
bendudler <ben.dudler@gmail.com>
2018-06-02 13:47:47 +0200

    sort file tree

--- src/app/recordings-list/recordings-list.component.ts
+++ src/app/recordings-list/recordings-list.component.ts
@@ -3,22 +3,21 @@ import {BehaviorSubject, Observable, of} from 'rxjs';
 import {FlatTreeControl} from '@angular/cdk/tree';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material';
 
-/**
- * File node data with nested structure.
- * Each node has a filename, and a type or a list of children.
- */
 export class FileNode {
   children: FileNode[];
   filename: string;
-  type: any;
+  metadata: any;
 }
 
-/** Flat node with expandable and level information */
 export class FileFlatNode {
   filename: string;
-  type: any;
+  metadata: any;
   level: number;
   expandable: boolean;
+}
+
+enum SORT_WAY {
+  ASC, DESC
 }
 
 @Injectable()
@@ -32,37 +31,45 @@ export class FileDatabase {
   }
 
   initialize() {
-    // Parse the string to json object.
     const dataObject = JSON.parse(TREE_DATA);
 
-    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
-    //     file node as children.
     const data = this.buildFileTree(dataObject, 0);
-
-    // Notify the change.
-    this.dataChange.next(data);
+    this.dataChange.next(this.sortBy(data, 'filename', SORT_WAY.DESC));
   }
 
-  /**
-   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
-   * The return value is the list of `FileNode`.
-   */
   buildFileTree(value: any, level: number): FileNode[] {
-    const data: any[] = [];
-    for (const k in value) {
-      const v = value[k];
+    const data: FileNode[] = [];
+    Object.entries(value).forEach(keyValue => {
       const node = new FileNode();
-      node.filename = `${k}`;
-      if (v === null || v === undefined) {
-        // no action
-      } else if (typeof v === 'object') {
-        node.children = this.buildFileTree(v, level + 1);
+      node.filename = `${keyValue[0]}`;
+      if (typeof keyValue[1] === 'object') {
+        node.children = this.buildFileTree(keyValue[1], level + 1);
       } else {
-        node.type = v;
+        node.metadata = keyValue[1];
       }
       data.push(node);
-    }
+    });
+
     return data;
+  }
+
+  private sortBy(data: FileNode[], key: string, sortWay: SORT_WAY = SORT_WAY.ASC): FileNode[] {
+    if (!Array.isArray(data)) {
+      return data;
+    }
+
+    const applySortWay = (a, way) => way === SORT_WAY.ASC ? a : a * -1;
+    const compareNumbers = (a, b) => a - b;
+    const compareStrings = (a, b) => a < b ? -1 : a > b ? 1 : 0;
+
+    const sortAlgorithmToUse = (a, b, way) => applySortWay(
+      Number.isInteger(data[0][key]) ? compareNumbers(a, b) : compareStrings(a, b), way
+    );
+
+    return data
+      .map(node => node[key])
+      .sort((a, b) => sortAlgorithmToUse(a, b, sortWay))
+      .map(keyValue => data.find(node => node[key] === keyValue));
   }
 }
 
@@ -81,7 +88,7 @@ export class RecordingsListComponent {
     const transformer = (node: FileNode, level: number) => {
       const flatNode = new FileFlatNode();
       flatNode.filename = node.filename;
-      flatNode.type = node.type;
+      flatNode.metadata = node.metadata;
       flatNode.level = level;
       flatNode.expandable = !!node.children;
       return flatNode;
@@ -107,100 +114,85 @@ export class RecordingsListComponent {
 /**
  * The file structure tree data in string. The data could be parsed into a Json object
  */
-const TREE_DATA = `
-  {
-    "2017": {
-      "November": {
-        "05": {
-          "Aufnahme 1": {
-            "Gesang Robin": {
-              "id": 12,
-              "size": 2345345
-            },
-            "Gesang Beni": {
-              "id": 13,
-              "size": 2698567
-            },
-            "Gesang Silvan": {
-              "id": 14,
-              "size": 3456345
-            }
-          }
-        }
+const TREE_DATA = `{
+  "20171105": {
+    "Aufnahme 1": {
+      "Gesang Robin": {
+        "id": 12,
+        "size": 2345345
       },
-      "Dezember": {
-        "08": {
-          "Aufnahme 1": {
-            "Gesang Robin": {
-              "id": 22,
-              "size": 2345345
-            },
-            "Gesang Beni": {
-              "id": 23,
-              "size": 2698567
-            },
-            "Gesang Silvan": {
-              "id": 24,
-              "size": 3456345
-            }
-          }
-        }
-      }
-    },
-    "2018": {
-      "Januar": {
-        "15": {
-          "Aufnahme 1": {
-            "Gesang Robin": {
-              "id": 32,
-              "size": 2345345
-            },
-            "Gesang Beni": {
-              "id": 33,
-              "size": 2698567
-            },
-            "Gesang Silvan": {
-              "id": 34,
-              "size": 3456345
-            }
-          }
-        }
+      "Gesang Beni": {
+        "id": 13,
+        "size": 2698567
       },
-      "Februar": {
-        "25": {
-          "Aufnahme 1": {
-            "Gesang Robin": {
-              "id": 42,
-              "size": 2345345
-            },
-            "Gesang Beni": {
-              "id": 443,
-              "size": 2698567
-            },
-            "Gesang Silvan": {
-              "id": 44,
-              "size": 3456345
-            }
-          }
-        }
-      },
-      "März": {
-        "16": {
-          "Aufnahme 1": {
-            "Gesang Robin": {
-              "id": 52,
-              "size": 2345345
-            },
-            "Gesang Beni": {
-              "id": 53,
-              "size": 2698567
-            },
-            "Gesang Silvan": {
-              "id": 54,
-              "size": 3456345
-            }
-          }
-        }
+      "Gesang Silvan": {
+        "id": 14,
+        "size": 3456345
       }
     }
+  },
+  "20171208": {
+    "Aufnahme 1": {
+      "Gesang Robin": {
+        "id": 22,
+        "size": 2345345
+      },
+      "Gesang Beni": {
+        "id": 23,
+        "size": 2698567
+      },
+      "Gesang Silvan": {
+        "id": 24,
+        "size": 3456345
+      }
+    }
+  },
+  "20180115": {
+    "Aufnahme 1": {
+      "Gesang Robin": {
+        "id": 32,
+        "size": 2345345
+      },
+      "Gesang Beni": {
+        "id": 33,
+        "size": 2698567
+      },
+      "Gesang Silvan": {
+        "id": 34,
+        "size": 3456345
+      }
+    }
+  },
+  "20180225": {
+    "Aufnahme 1": {
+      "Gesang Robin": {
+        "id": 42,
+        "size": 2345345
+      },
+      "Gesang Beni": {
+        "id": 443,
+        "size": 2698567
+      },
+      "Gesang Silvan": {
+        "id": 44,
+        "size": 3456345
+      }
+    }
+  },
+  "20180316": {
+    "Aufnahme 1": {
+      "Gesang Robin": {
+        "id": 52,
+        "size": 2345345
+      },
+      "Gesang Beni": {
+        "id": 53,
+        "size": 2698567
+      },
+      "Gesang Silvan": {
+        "id": 54,
+        "size": 3456345
+      }
+    }
+  }
 }`;
